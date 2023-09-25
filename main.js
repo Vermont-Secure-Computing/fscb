35738fa69d3ecd6a50f02cb7b809a257fc8faba0
stuff-new <RobertStuff>
2023-09-25 14:26:46 +0800

    change banker selection

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const fs = require("fs");
 const jsonFile = "./data/data.json"
+const jsonFileBanker = "./data/banker.json"
 
 const isDev = process.env.NODE_ENV !== 'production';
 
@@ -24,6 +25,8 @@ const createWindow = () => {
   if (isDev) {
     win.webContents.openDevTools();
   }
+
+  win.removeMenu()
 
   const test = fs.readFileSync("./testing.json", "utf-8")
   // console.log(test)
@@ -63,6 +66,22 @@ function idNumber() {
 
 }
 
+function bankerIdNumber() {
+  try {
+    if (fs.existsSync(jsonFileBanker)) {
+      const dataJson = fs.readFileSync(jsonFileBanker, "utf-8")
+      const jconvert = JSON.parse(dataJson)
+      console.log("length: ", jconvert[Object.keys(jconvert)[Object.keys(jconvert).length - 1]].banker_id)
+      return jconvert[Object.keys(jconvert)[Object.keys(jconvert).length - 1]].banker_id + 1
+    } else {
+      return 1
+    }
+  } catch(err) {
+    console.error(err)
+  }
+
+}
+
 // idNumber()
 
 
@@ -71,42 +90,60 @@ ipcMain.on("message:contractnew", (e, options) => {
   // console.log("rederer send", options)
   const getIdNumber = idNumber()
   pathMessage = 'message'
-  // const contractTextReference = options.contractSendName +"-"+ options.creatorSendEmail +"-"+ "contract"+getIdNumber+".txt";
-  for (let i = 0; i < options.bankersMerge.length; i++) {
-    let textData = `contract_id: ${getIdNumber} \n
-    banker_id: ${options.bankersMerge[i].banker_id}
-    creator_name: ${options.creatorSendName} \n
-    creator_email: ${options.creatorSendEmail} \n
-    banker_name: ${options.bankersMerge[i].banker_name} \n
-    banker_email: ${options.bankersMerge[i].banker_email} \n
-    banker_respons_address: ${options.bankersMerge[i].banker_response_address}`
-    fs.mkdir(pathMessage, { recursive: true}, function (err) {
-      //     if (err) return err;
-      fs.writeFile(pathMessage +"/"+ options.bankersMerge[i].banker_request_address
-        , textData, function writeText() {
-        if (err) return console.log(err);
-        // console.log(JSON.stringify(sData));
-        console.log('writing to ' + options.bankersMerge[i].banker_request_address);
-      });
-    })
-
+  // console.log(options.bankersMerge)
+  const mybankers = fs.readFileSync("./data/banker.json", "utf-8")
+  const bankersParse = JSON.parse(mybankers)
+  // console.log(typeof(bankersParse))
+  let mergeBankers = []
+  for (let i = 0; i < options.bankersMerge.length; i++ ) {
+    for (let j in bankersParse) {
+      if (options.bankersMerge[i] === bankersParse[j].banker_email) {
+        bankersParse[j].signature = ""
+        mergeBankers.push(bankersParse[j])
+      }
+    }
   }
+  // }
+  // for (let i = 0; i < options.bankersMerge; i++ ) {
+  //   const result = testing.fil
+  // }
+  // const contractTextReference = options.contractSendName +"-"+ options.creatorSendEmail +"-"+ "contract"+getIdNumber+".txt";
+  // for (let i = 0; i < options.bankersMerge.length; i++) {
+  //   let textData = `contract_id: ${getIdNumber} \n
+  //   banker_id: ${options.bankersMerge[i].banker_id}
+  //   creator_name: ${options.creatorSendName} \n
+  //   creator_email: ${options.creatorSendEmail} \n
+  //   banker_name: ${options.bankersMerge[i].banker_name} \n
+  //   banker_email: ${options.bankersMerge[i].banker_email} \n
+  //   banker_respons_address: ${options.bankersMerge[i].banker_response_address}`
+  //   fs.mkdir(pathMessage, { recursive: true}, function (err) {
+  //     //     if (err) return err;
+  //     fs.writeFile(pathMessage +"/"+ options.bankersMerge[i].banker_request_address
+  //       , textData, function writeText() {
+  //       if (err) return console.log(err);
+  //       // console.log(JSON.stringify(sData));
+  //       console.log('writing to ' + options.bankersMerge[i].banker_request_address);
+  //     });
+  //   })
+
+  // }
 
   let data = {
+      "id": Math.floor(1000000000 + Math.random() * 9000000000),
       "contract_id": getIdNumber,
       // "txt_file_reference": contractTextReference,
       "contract_name": options.contractSendName,
       "creator_name": options.creatorSendName,
       "creator_email": options.creatorSendEmail,
-      "bankers": options.bankersMerge,
+      "bankers": mergeBankers,
       "signature_nedded": options.sigSendNumber,
-      "creator_pubkey": options.creatorAddressDetail.pubkey,
-      "creator_address": options.creatorAddressDetail.address,
-      "creator_wif": options.creatorAddressDetail.wif,
-      "creator_privkey": options.creatorAddressDetail.privkey,
-         "claimed": "false"
+      // "creator_pubkey": options.creatorAddressDetail.pubkey,
+      // "creator_address": options.creatorAddressDetail.address,
+      // "creator_wif": options.creatorAddressDetail.wif,
+      // "creator_privkey": options.creatorAddressDetail.privkey,
+      "claimed": "false"
   }
-  // const sData = JSON.stringify(data, null, 2)
+  const sData = JSON.stringify(data, null, 2)
   const fileName = "data.json"
   const path = "data"
   try {
@@ -117,7 +154,7 @@ ipcMain.on("message:contractnew", (e, options) => {
           jdata = JSON.parse(jdata);
           //Step 3: append contract variable to list
           jdata["contract" + getIdNumber] = data
-          console.log(jdata);
+          // console.log(jdata);
           const wData = JSON.stringify(jdata, null, 2)
           fs.writeFile(path +"/"+ fileName, wData, function writeJson() {
             if (err) return console.log(err);
@@ -132,7 +169,7 @@ ipcMain.on("message:contractnew", (e, options) => {
           , sData, function writeJson() {
           if (err) return console.log(err);
           // console.log(JSON.stringify(sData));
-          console.log('writing to ' + fileName);
+          // console.log('writing to ' + fileName);
         });
       }
     });
@@ -140,6 +177,62 @@ ipcMain.on("message:contractnew", (e, options) => {
     console.log(e)
   }
   console.log("Success")
+})
+
+ipcMain.on('message:addBanker', (e, options) => {
+  console.log(options)
+  const getBankerIdNumber = bankerIdNumber()
+  let data = {
+    "id": Math.floor(1000000000 + Math.random() * 9000000000),
+    "banker_id": getBankerIdNumber,
+    "banker_name": options.bankerName,
+    "banker_email": options.bankerEmail,
+    "pubkey": ""
+  }
+  const fileName = "banker.json"
+  const path = "data"
+  try {
+    fs.mkdir(path, { recursive: true}, function (err) {
+      if (err) return err;
+      if (fs.existsSync(jsonFileBanker)) {
+        fs.readFile(path +"/"+ fileName, 'utf8', function(err, jdata){
+          jdata = JSON.parse(jdata);
+          //Step 3: append contract variable to list
+          jdata["banker" + getBankerIdNumber] = data
+          console.log(jdata);
+          const wData = JSON.stringify(jdata, null, 2)
+          fs.writeFile(path +"/"+ fileName, wData, function writeJson() {
+            if (err) return console.log(err);
+          })
+      });
+      } else {
+        const addBanker = {
+          ["banker" + getBankerIdNumber]: data
+        }
+        const sData = JSON.stringify(addBanker, null, 2)
+        fs.writeFile(path +"/"+ fileName
+          , sData, function writeJson() {
+          if (err) return console.log(err);
+          // console.log(JSON.stringify(sData));
+          console.log('writing to ' + fileName);
+        });
+      }
+    });
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+ipcMain.on('click:addBanker', () => {
+  const fileName = "banker.json"
+  const path = "data"
+  fs.readFile(path +"/"+ fileName, 'utf8', function(err, jdata){
+    jdata = JSON.parse(jdata);
+    //Step 3: append contract variable to list
+    // console.log(jdata);
+    // const wData = JSON.stringify(jdata, null, 2)
+    win.webContents.send('send:bankers', jdata)
+    })
 })
 
 // async function loadListFile() {
--- src/js/renderer.js
+++ src/js/renderer.js
@@ -3,6 +3,8 @@
 
 
 
+
+
 //  var multisig =  coinjs.pubkeys2MultisigAddress(keys, sigsNeeded);
 //  console.log(multisig)
  
@@ -12,16 +14,19 @@ const importText =   document.getElementById('import-text');
 // const bankersClick = document.getElementById('bakers-address').getElementsByClassName('pubkeyAdd')[0];
 
 const formCreateAccount =   document.getElementById('create-new-form');
+const formAddBanker = document.getElementById('add-banker-form');
 const contractName = document.getElementById("contract-name");
 const creatorName = document.getElementById("creator-name");
 const creatorEmail = document.getElementById("creator-email");
 const creatorAddress = document.getElementById("creator-address");
 const minusButton = document.getElementById('multikeys');
 const addMinus = document.getElementById('multikeys').getElementsByClassName('pubkeyAdd')[0]
+const testingClick = document.getElementsByClassName('testingClick')[0]
 
 const sigNumber = document.getElementById('releaseCoins');
 let tabsContainer = document.getElementById('tabs');
 let tabTogglers = tabsContainer.querySelectorAll("nav #tabs a");
+let bankersArray
 
 
 
@@ -75,19 +80,21 @@ async function saveAndCreateText(e) {
     const creatorAddressDetail = await coinjs.newKeys()
     // console.log("New Key ", creatorAddressDetail)
     const sigSendNumber = sigNumber.options[sigNumber.selectedIndex].text;
-    const innerMultiKey = document.querySelectorAll('.multikeysInner')
+    const innerMultiKey = document.querySelectorAll('.active a')
+    console.log(innerMultiKey)
     let bankersMerge = []
     for (let i = 0; i < innerMultiKey.length; i++) {
-        bankersMerge.push({"banker_id": contractSendName.split(' ').join('_') + "-" + innerMultiKey[i].querySelector('.banker-email').value + "-" + (Math.floor(1000000000 + Math.random() * 9000000000)), 
-        "banker_name": innerMultiKey[i].querySelector('.banker-name').value, 
-        "banker_email": innerMultiKey[i].querySelector('.banker-email').value, 
-        "banker_address": "",
-        "banker_signature": "",
-        "banker_request_address": "banker-request-address" + "-" + innerMultiKey[i].querySelector('.banker-email').value + "-" + (Math.floor(1000000000 + Math.random() * 9000000000)) + ".txt",
-        "banker_response_address": "banker-response-address" + "-" + innerMultiKey[i].querySelector('.banker-email').value + "-" + (Math.floor(1000000000 + Math.random() * 9000000000)) + ".txt",
-        "banker_request_signature": "banker-request-signature" + "-" + innerMultiKey[i].querySelector('.banker-email').value + "-" + (Math.floor(1000000000 + Math.random() * 9000000000)) + ".txt",
-        "banker_response_signature": "banker-response-signature" + "-" + innerMultiKey[i].querySelector('.banker-email').value + "-" + (Math.floor(1000000000 + Math.random() * 9000000000)) + ".txt"
-    })
+    //     bankersMerge.push({"banker_id": contractSendName.split(' ').join('_') + "-" + innerMultiKey[i].querySelector('.banker-email').value + "-" + (Math.floor(1000000000 + Math.random() * 9000000000)), 
+    //     "banker_name": innerMultiKey[i].querySelector('.banker-name').value, 
+    //     "banker_email": innerMultiKey[i].querySelector('.banker-email').value, 
+    //     "banker_address": "",
+    //     "banker_signature": "",
+    //     "banker_request_address": "banker-request-address" + "-" + innerMultiKey[i].querySelector('.banker-email').value + "-" + (Math.floor(1000000000 + Math.random() * 9000000000)) + ".txt",
+    //     "banker_response_address": "banker-response-address" + "-" + innerMultiKey[i].querySelector('.banker-email').value + "-" + (Math.floor(1000000000 + Math.random() * 9000000000)) + ".txt",
+    //     "banker_request_signature": "banker-request-signature" + "-" + innerMultiKey[i].querySelector('.banker-email').value + "-" + (Math.floor(1000000000 + Math.random() * 9000000000)) + ".txt",
+    //     "banker_response_signature": "banker-response-signature" + "-" + innerMultiKey[i].querySelector('.banker-email').value + "-" + (Math.floor(1000000000 + Math.random() * 9000000000)) + ".txt"
+    // })
+        bankersMerge.push(innerMultiKey[i].dataset.value)
     }
 
     ipcRenderer.send("message:contractnew", {
@@ -119,24 +126,45 @@ ipcRenderer.on("list:file", function(evt){
 })
 
 function addOrDelete() {
+    console.log("click add or delete")
     const mainKey = document.getElementById('multikeys')
     let displayButton = document.querySelector("form button");
-    const bankers = document.querySelectorAll('.banker')
-    if (bankers.length >= 28 ) return;
+    // const bankers = document.querySelectorAll('.banker')
+    // if (bankers.length >= 28 ) return;
     // innerKey.getElementsByClassName('green')[0]
     // const clone = '<div class="grid md:grid-cols-7 md:gap-6" id="multikeysInner">'+innerKey.innerHTML+'</div>'
     // mainKey.innerHTML += clone
     // document.getElementById('multikeys', 'img-anchor').dataset('./assets/imgs/minus.svg')
     let div = document.createElement('div');
     div.setAttribute("class", "grid md:grid-cols-7 md:gap-6 multikeysInner");
-    let input1 = document.createElement('input')
-    input1.setAttribute('class', 'col-span-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 banker-name')
-    input1.setAttribute('placeholder', 'Bankers Name')
-    input1.setAttribute('required', '')
-    let input2 = document.createElement('input')
-    input2.setAttribute('class', 'col-span-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 banker-email')
-    input2.setAttribute('placeholder', 'Bankers Email')
-    input2.setAttribute('required', '')
+    // let input1 = document.createElement('input')
+    // input1.setAttribute('class', 'col-span-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 banker-name')
+    // input1.setAttribute('placeholder', 'Bankers Name')
+    // input1.setAttribute('required', '')
+    // let input2 = document.createElement('input')
+    // input2.setAttribute('class', 'col-span-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 banker-email')
+    // input2.setAttribute('placeholder', 'Bankers Email')
+    // input2.setAttribute('required', '')
+    let select = document.createElement('select')
+    select.setAttribute('name', 'banker-name1')
+    select.setAttribute('id', 'bankers-name')
+    select.setAttribute('class', 'col-span-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500')
+    let option = document.createElement('option')
+    option.setAttribute('value', '')
+    option.setAttribute('disabled', 'disabled')
+    option.setAttribute('hidden', 'hidden')
+    option.setAttribute('selected', 'selected')
+    option.innerHTML = "Choose Banker"
+    select.appendChild(option)
+    // option.setAttribute('data-dept', 'Choose Banker')
+    for (const key in bankersArray) {
+        const opt = bankersArray[key].banker_email
+        console.log(opt)
+        let option1 = document.createElement('option')
+        option1.textContent = opt;
+        option1.value = opt;
+        select.appendChild(option1)
+    }
     let anchor = document.createElement('a')
     anchor.setAttribute('class', 'pubkeyRemove')
     let minus = document.createElement('object')
@@ -145,8 +173,8 @@ function addOrDelete() {
     minus.setAttribute('height', '50')
     minus.setAttribute('class', 'red')
     anchor.appendChild(minus)
-    div.appendChild(input1)
-    div.appendChild(input2)
+    div.appendChild(select)
+    // div.appendChild(input2)
     div.appendChild(anchor)
     mainKey.appendChild(div)
 }
@@ -158,10 +186,234 @@ function deleteInput(e) {
     document.getElementById('multikeys').removeChild(removeEl);
 }
 
-importText.addEventListener("change", loadText)
-formCreateAccount.addEventListener("submit", saveAndCreateText)
-addMinus.addEventListener('click', addOrDelete)
-minusButton.addEventListener('click', deleteInput)
+function addBanker(e) {
+    e.preventDefault()
+    // const mainBankerDiv = document.getElementById('addbanker')
+    const bankerName = document.getElementById('banker-name-add').value
+    const bankerEmail = document.getElementById('banker-email-add').value
+    // console.log(mainBankerDiv)
+    ipcRenderer.send('message:addBanker', {
+        bankerName,
+        bankerEmail
+    })
+}
+
+function getBanker() {
+    if (!bankersArray) {
+        ipcRenderer.send('click:addBanker', {})
+    }
+}
+
+ipcRenderer.on('send:bankers', function(e) {
+    const select = document.querySelector("select[multiple]");
+    console.log(select)
+    bankersArray = e
+    for(const key in bankersArray) {
+        // console.log(bankersArray[key])
+        // let data = {
+        //     "banker_name": bankersArray[key].banker_name
+        // }
+        const opt = bankersArray[key].banker_email;
+        // console.log(opt)
+        const el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        el.setAttribute('class', 'optionForSelection')
+        select.appendChild(el);
+    }
+
+    // select multiple
+
+// let select = document.getElementById('bankers-name');
+    const selectOptions = select.querySelectorAll('option');
+    // console.log(selectOptions)
+
+    const newSelect = document.createElement('div');
+    newSelect.classList.add('selectMultiple');
+    const active = document.createElement('div');
+    active.classList.add('active');
+    const optionList = document.createElement('ul');
+    const placeholder = select.dataset.placeholder;
+
+    const span = document.createElement('span');
+    span.innerText = placeholder;
+    active.appendChild(span);
+
+    selectOptions.forEach((option) => {
+        console.log(option)
+        let text = option.innerText;
+        if(option.selected){
+            let tag = document.createElement('a');
+            tag.dataset.value = option.value;
+            tag.innerHTML = "<em>"+text+"</em><i></i>";
+            active.appendChild(tag);
+            span.classList.add('hide');
+        }else{
+            let item = document.createElement('li');
+            item.dataset.value = option.value;
+            item.innerHTML = text;
+            optionList.appendChild(item);
+        }
+    });
+    const arrow = document.createElement('div');
+    arrow.classList.add('arrow');
+    active.appendChild(arrow);
+
+    newSelect.appendChild(active);
+    newSelect.appendChild(optionList);
+
+    select.parentElement.append(newSelect);
+    span.appendChild(select);
+
+    // newSelect.appendChild(select);
+    //select.style.display = "none";
+    //1
+    //document.querySelectorAll('.selectMultiple ul li').forEach((li) => {
+    document.querySelector('.selectMultiple ul').addEventListener('click', (e) => {
+        let li = e.target.closest('li');
+        if(!li){return;}
+        let select = li.closest('.selectMultiple');
+        if(!select.classList.contains('clicked')){
+            select.classList.add('clicked');
+            if(li.previousElementSibling){
+                li.previousElementSibling.classList.add('beforeRemove');
+            }
+            if(li.nextElementSibling){
+                li.nextElementSibling.classList.add('afterRemove');
+            }
+            li.classList.add('remove');
+            let a = document.createElement('a');
+            a.dataset.value = li.dataset.value;
+            a.innerHTML = "<em>"+li.innerText+"</em><i></i>";
+            a.classList.add('notShown');
+            // a.style.display = "none";
+            select.querySelector('div').appendChild(a); //might have to check later
+            let selectEl = select.querySelector('select');
+            let opt = selectEl.querySelector('option[value="'+li.dataset.value+'"]');
+            opt.setAttribute('selected', 'selected');
+            setTimeout(() => {
+                a.classList.add('shown');
+                select.querySelector('span').classList.add('hide');
+                // if(select.querySelector('option').innerText == li.innerText){
+                // 	select.querySelector('option').selected
+                // }
+
+            }, 300);
+            //1st
+            setTimeout(() => {
+                let styles = window.getComputedStyle(li);
+                    let liHeight = styles.height;
+                    let liPadding = styles.padding;
+                    let removing = li.animate([
+                        {
+                            height: liHeight,
+                            padding: liPadding
+                        },
+                        {
+                            height: '0px',
+                            padding: '0px'
+                        }
+                    ], {
+                        duration: 300, easing: 'ease-in-out'
+                    });
+                    removing.onfinish = () => {
+                        if(li.previousElementSibling){
+                            li.previousElementSibling.classList.remove('beforeRemove');
+                        }
+                        if(li.nextElementSibling){
+                            li.nextElementSibling.classList.remove('afterRemove');
+                        }
+                        li.remove();
+                        select.classList.remove('clicked');
+                    }
+    //             setTimeout(() => {
+    //                 if(li.previousElementSibling){
+    //                     li.previousElementSibling.classList.remove('beforeRemove');
+    //                 }
+    //                 if(li.nextElementSibling){
+    //                     li.nextElementSibling.classList.remove('afterRemove');
+    //                 }
+                    
+    //             }, 200);
+            }, 300); //600
+                //2nd
+        }
+    });
+    //2
+    //document.querySelectorAll('.selectMultiple > div a').forEach((a) => {
+    document.querySelector('.selectMultiple > div').addEventListener('click', (e) => {
+        let a = e.target.closest('a');
+        let select = e.target.closest('.selectMultiple');
+        if(!a){return;}
+        a.className = '';
+        a.classList.add('remove');
+        select.classList.add('open');
+        let selectEl = select.querySelector('select');
+        let opt = selectEl.querySelector('option[value="'+a.dataset.value+'"]');
+        opt.removeAttribute('selected');
+        //setTimeout(() => {
+            a.classList.add('disappear');
+            setTimeout(() => {
+                // start animation
+                let styles = window.getComputedStyle(a);
+                let padding = styles.padding;
+                let deltaWidth = styles.width;
+                let deltaHeight = styles.height;
+
+                let removeOption = a.animate([
+                    {
+                        width: deltaWidth,
+                        height: deltaHeight,
+                        padding: padding
+                    },
+                    {
+                        width: '0px',
+                        height: '0px',
+                        padding: '0px'
+                    }
+                ], {
+                    duration: 0,
+                    easing: 'ease-in-out'
+                });
+                
+                let li = document.createElement('li');
+                li.dataset.value = a.dataset.value;
+                li.innerText = a.querySelector('em').innerText;
+                li.classList.add('show');
+                select.querySelector('ul').appendChild(li);
+                setTimeout(() => {
+                    if(!selectEl.selectedOptions.length){
+                        select.querySelector('span').classList.remove('hide');
+                    }
+                    li.className = '';
+                }, 350);
+                
+                removeOption.onfinish = () => {
+                    a.remove();
+                }
+                //end animation
+
+            }, 300);
+        //}, 400);
+    });
+    //});
+    //3
+    document.querySelectorAll('.selectMultiple > div .arrow, .selectMultiple > div span').forEach((el) => {
+        el.addEventListener('click', (e) => {
+            el.closest('.selectMultiple').classList.toggle('open');
+        });
+    });
+    
+})
+
+
+
+// importText.addEventListener("change", loadText);
+formCreateAccount.addEventListener("submit", saveAndCreateText);
+// addMinus.addEventListener('click', addOrDelete);
+minusButton.addEventListener('click', deleteInput);
+formAddBanker.addEventListener('submit', addBanker);
+testingClick.addEventListener('click', getBanker)
 
 
 
